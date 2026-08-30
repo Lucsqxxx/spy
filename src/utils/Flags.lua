32f82bab70fa785d38d9b4223996d8555279e186
Lucsqx <waydekurt@gmail.com>
2026-08-30 22:46:52 +0800

    Comment strip

--- src/utils/Flags.lua
+++ src/utils/Flags.lua
@@ -13,14 +13,14 @@ type table = {
 
 local Module = {
     Flags = {
-        -- PreventRenaming = {
-        --     Value = false,
-        --     Label = "No renaming",
-        -- },
-        -- PreventParenting = {
-        --     Value = false,
-        --     Label = "No parenting",
-        -- },
+        
+        
+        
+        
+        
+        
+        
+        
         NoComments = {
             Value = false,
             Label = "No comments",
@@ -29,7 +29,7 @@ local Module = {
             Value = false,
             Label = "Auto-open newest log",
         },
-        DecompilePopout = { -- Lovre SHUSH
+        DecompilePopout = { 
             Value = false,
             Label = "Pop-out decompiles",
         },
